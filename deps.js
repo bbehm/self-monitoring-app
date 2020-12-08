4b03efa2ec483cc357de9c10bb01cfddf2ae3159
Bianca Behm <bbehm@student.hive.fi>
2020-12-08 11:26:38 +0200

    added superoak

--- deps.js
+++ deps.js
@@ -5,3 +5,4 @@ export { Session } from "https://deno.land/x/session@v1.0.0/mod.ts";
 export { decode } from "https://deno.land/std@0.65.0/encoding/utf8.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { superoak } from "https://deno.land/x/superoak@2.3.1/mod.ts";

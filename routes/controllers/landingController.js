7e731947653a91353d9e1aece095e8382dbb7e4e
Bianca Behm <bbehm@student.hive.fi>
2020-12-10 19:16:29 +0200

    makeover and copywrite

--- routes/controllers/landingController.js
+++ routes/controllers/landingController.js
@@ -1,5 +1,39 @@
+import { getCommunityDailyReport } from "../../services/dataServices.js";
+
 const landingPage = async({render}) => {
-	render('landing.ejs');
+	const day = new Date();
+	const yesterday = new Date(day - 86400000);
+	const formatDate = day.toISOString().substring(0,10);
+	const formatYesterday = yesterday.toISOString().substring(0,10);
+	const communityDailyReport = await getCommunityDailyReport(formatDate);
+	const communityYesterdayReport = await getCommunityDailyReport(formatYesterday);
+	const data = {
+		communityMorning: communityDailyReport && communityDailyReport.morning,
+		communityEvening: communityDailyReport && communityDailyReport.evening
+	};
+	const yesterdayData = {
+		communityMorning: communityYesterdayReport && communityYesterdayReport.morning,
+		communityEvening: communityYesterdayReport && communityYesterdayReport.evening
+	};
+	if (data.communityMorning && data.communityEvening) {
+		data.communityMoodToday = (Number(communityDailyReport.morning + communityDailyReport.evening) / 2);
+	} else if (data.communityMorning) {
+		data.communityMoodToday = Number(communityDailyReport.morning);
+	} else if (data.communityEvening) {
+		data.communityMoodToday = Number(communityDailyReport.evening);
+	} else {
+		data.communityMoodToday = 'No reported mood for today';
+	}
+	if (yesterdayData.communityMorning && yesterdayData.communityEvening) {
+		data.communityMoodYesterday = (Number(communityYesterdayReport.morning + communityYesterdayReport.evening) / 2);
+	} else if (yesterdayData.communityMorning) {
+		data.communityMoodYesterday = Number(communityYesterdayReport.morning);
+	} else if (yesterdayData.communityEvening) {
+		data.communityMoodYesterday = Number(communityYesterdayReport.evening);
+	} else {
+		data.communityMoodYesterday = 'No reported mood for yesterday';
+	}
+	render('landing.ejs', data);
 }
 
 export { landingPage };
